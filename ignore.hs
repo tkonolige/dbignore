9e44d9b25333b57f7ee33178f4e351ef9fad2f60
Tristan Konolige <tristan.konolige@gmail.com>
2014-08-27 11:53:47 -0700

    Use fnmatch to match patterns against files. fixed #8, working on #5

--- ignore.hs
+++ ignore.hs
@@ -17,7 +17,7 @@ import Foreign.C.String
 
 import System.Directory
 import System.Posix.FilePath
-import System.FilePath.Glob
+import qualified System.FilePath.Glob as G
 
 import Data.ByteString as B
 import Data.ByteString.Char8 as B8 (pack, unpack, split, lines)
@@ -38,18 +38,30 @@ import Data.Aeson
 import Data.HashMap.Strict
 
 
+newtype Pattern = Pattern ByteString deriving (Show)
+
 -- global cache of dbignore files
+-- TODO: merge into config?
 cacheVar :: MVar (Trie [Pattern])
 {-# NOINLINE cacheVar #-}
 cacheVar = unsafePerformIO (newMVar T.empty)
 
+-- holds whether or not dbignore has been initialized yet.
+-- would like to initialize using main, however dbignore loads early and will crash
 initialized :: MVar Bool
 {-# NOINLINE initialized #-}
 initialized = unsafePerformIO (newMVar False)
 
-data Config = Config { dbPath :: RawFilePath
-                     , dbName :: ByteString
-                     , debug  :: Bool
+-- global config
+-- multiple threads use this
+config :: MVar Config
+{-# NOINLINE config #-}
+config = unsafePerformIO (newMVar defaultConfig)
+
+
+data Config = Config { dbPath :: RawFilePath -- where the Dropbox folder is located
+                     , dbName :: ByteString  -- name of ignore file, defaults to .dbignore
+                     , debug  :: Bool        -- enable debug mode
                      }
                      deriving (Show)
 
@@ -62,16 +74,14 @@ instance FromJSON Config where
 
 defaultConfig = Config { dbPath = B.empty, dbName = ".dbignore", debug = False }
 
-config :: MVar Config
-{-# NOINLINE config #-}
-config = unsafePerformIO (newMVar defaultConfig)
-
+-- for debugging, appends to dbignore_log
 debugMsg :: Config -> String -> IO ()
 debugMsg conf msg = handle (\(e :: IOException) -> return ()) $ do -- TODO: write concurrently
   if debug conf then do
     Prelude.appendFile (B8.unpack (dbPath conf) ++ "/../dbignore_log") (msg ++ "\n")
   else
     return ()
+
 
 -- figure out dropbox directory
 -- Dropbox stores its location inside ~/.dropbox/info.json
@@ -85,16 +95,31 @@ getDropboxPath = do
       String path = pp ! "path"
   return $ B8.pack $ Text.unpack path
 
+-- check if file is an ignore file
 isDBIgnore :: Config -> RawFilePath -> Bool
 isDBIgnore conf = isSuffixOf (dbName conf)
+
+foreign import ccall "fnmatch" c_fnmatch :: CString -> CString -> CInt -> IO (CInt)
+
+-- calls fnmatch
+fnmatch :: Pattern -> ByteString -> IO (Bool)
+fnmatch (Pattern pattern) string = 
+  useAsCString pattern (\p ->
+    useAsCString string (\s ->
+      c_fnmatch p s 0x2 >>= \case -- FNM_PATHNAME is 0x2
+        0 -> return True
+        _ -> return False
+    )
+  )
 
 -- add patterns in an ignore file to the cache
 addIgnore :: RawFilePath -> Trie [Pattern] -> IO (Trie [Pattern])
 addIgnore file trie = handle (\(e :: IOException) -> return trie) $ do
   regexs <- B.readFile $ B8.unpack file
-  let splits = Prelude.map (compile . B8.unpack . (takeDirectory file `append` "/" `append`)) $ B8.lines regexs
+  let splits = Prelude.map (Pattern . (takeDirectory file `append` "/" `append`)) $ B8.lines regexs
   return $ T.insert (takeDirectory file) splits trie
 
+-- read config from disk
 readConfig :: RawFilePath -> IO Config
 readConfig dbpath = handle (\(e :: IOException) -> return defaultConfig) $ do
   conf <- B.readFile $ B8.unpack $ dbpath `append` "/.dbignore_config"
@@ -107,8 +132,10 @@ initialize = do
   dbpath <- getDropboxPath
   conf <- readConfig dbpath
   debugMsg conf "Initializing"
+  -- put config in global
   modifyMVar_ config $ const $ return conf
-  ignores <- globDir1 (compile $ "**/" ++ B8.unpack (dbName conf)) $ B8.unpack dbpath
+  -- find all ignore files and initialize cache with them
+  ignores <- G.globDir1 (G.compile $ "**/" ++ B8.unpack (dbName conf)) $ B8.unpack dbpath
   debugMsg conf $ "Found ignore files:"
   mapM_ (debugMsg conf . (++) "  ") ignores
   modifyMVar_ cacheVar $ \cache ->
@@ -116,6 +143,7 @@ initialize = do
 
 -- the main ignore function
 -- determines if a filepath should be ignored or not
+-- TODO: maybe use config with state monad? need mvar though
 ignore :: RawFilePath -> IO Bool
 ignore file = do
   modifyMVar_ initialized $ \case
@@ -140,12 +168,12 @@ ignore file = do
                       where
                         doesMatch :: [Pattern] -> IO Bool -- TODO: looks like a fold
                         doesMatch (r:rs) = do
-                          case match r (B8.unpack file) of 
+                          fnmatch r file >>= \case
                             True  -> do
-                              debugMsg conf $ "    " ++ decompile r ++ " matches!"
+                              debugMsg conf $ "    " ++ show r ++ " matches!"
                               return True
                             False -> do
-                              debugMsg conf $ "    " ++ decompile r ++ " does not match"
+                              debugMsg conf $ "    " ++ show r ++ " does not match"
                               doesMatch rs
                         doesMatch [] = return False
                      Nothing -> do
@@ -154,13 +182,13 @@ ignore file = do
         return res
     False -> return False
 
-boolToCInt :: Bool -> CInt
-boolToCInt b = case b of
-                 True  -> 1
-                 False -> 0
 
 -- c wrapper for ignore
 ignore_hs :: CString -> IO CInt
 ignore_hs str = packCString str >>= (liftM boolToCInt) . ignore
+  where
+    boolToCInt b = case b of
+                     True  -> 1
+                     False -> 0
 
 foreign export ccall ignore_hs :: CString -> IO CInt
